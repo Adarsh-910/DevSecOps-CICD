330a68dbb2d71d66d7ea9f75d65e9c2a6dd0680a
Adarsh-910 <160036836+Adarsh-910@users.noreply.github.com>
2025-07-27 22:55:47 +0530

    fix: Secure S3 bucket permissions

--- main.tf
+++ main.tf
@@ -6,6 +6,4 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   bucket = "your-unique-devsecops-bucket-2025" # Change to a unique name
   
-  # WARNING: This line is insecure and will be flagged by tfsec.
-  acl = "public-read" 
 }
